125f84d87c28b8f091f7a8cdb83a2bc92188881e
Navdish <navdishjaggi@gmail.com>
2024-01-18 17:58:27 +0530

    changes

---
+++ client/src/App.js
@@ -0,0 +1,94 @@
+import './App.css';
+import React, { useState } from 'react';
+
+
+function App() {
+  const [revalue, setRevalue] = useState("");
+  const [tick, setTick] = useState(true);
+  const [value, setValue] = useState("");
+  const layout = {
+    value : "",
+    tick : Number,
+    counter : Number
+  };
+  const [todo, setTodo] = useState([layout]);
+  
+
+  function handleAdd(e) {
+    e.preventDefault();
+    
+    setTodo(todo => [...todo, {value: value, tick : 0, counter : 0}]);
+    setValue("");
+    console.log(todo);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+       To-do list
+      </header>
+      <ul className='App-list'>
+        {todo.map((x, key)=> { 
+          if(x.counter === 0 ) {
+              return <li key={key}>
+                <input type='checkbox' checked={x.tick}  onChange={(e) => { x.tick =  1; x.counter = 1; setTick(!tick);}} />
+                 {x.value} 
+                <button onClick={(e) => { x.counter = 2; setTick((!tick));}}>
+                  Edit
+                </button>
+                <button onClick={(e) => {x.counter = 3; setTick(!tick);}}>
+                  Deleted
+                </button>
+              </li>  
+          }
+          else if( x.counter === 1) {
+            return <li className='Completed-text' key={key}>
+              <input type='checkbox' checked={x.tick}  onChange={(e) => { x.tick = 0; x.counter = 0; setTick(!tick);  }} />
+               {x.value} 
+              <button onClick={(e) => { x.counter = 2; setTick((!tick));}}>
+                Edit
+              </button>
+              <button onClick={(e) => {x.counter = 3; setTick(!tick);}}>
+                Deleted
+              </button>
+              </li>
+          }
+          else if( x.counter === 2) {
+            return <li key={key}>
+              <form >
+                <input type='checkbox' checked={x.tick} disabled/>
+                <input type='text' onChange={(e)=> { setRevalue(e.target.value)}}/>
+                <button type='submit' onClick={(e)=> {e.preventDefault(); x.value=revalue; x.counter=x.tick; setTick(!tick);}}>
+                  Make Edits
+                </button>
+                <button onClick={(e) => {x.counter = 3; setTick(!tick)}} disabled>
+                  Deleted
+                </button>
+              </form>
+              </li>
+              
+          }
+          else {
+            setTodo(todo => todo.filter((y)=> {
+              return y.value !== x.value;
+            })); 
+          }
+          
+          })
+        }
+      </ul>
+      <form >
+        <input className="Add-input" type="text" value={value} onChange={(e)=> setValue(e.target.value)} placeholder='Enter the new entry'/>
+        <button className="Add-button" type='submit' onClick={(e)=>handleAdd(e)}>+</button>
+      </form>
+
+      
+      
+    </div>
+  );
+}
+
+export default App;
+
+
+  
