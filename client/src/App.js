175e68f4c10757209ad1cf42145b8d385a74fdd7
Navdish <navdishjaggi@gmail.com>
2024-01-22 11:40:35 +0530

    third-commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useId } from 'react';
+import React, { useState,useEffect, useId } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 
@@ -9,19 +9,62 @@ function App() {
   const [value, setValue] = useState("");
   const layout = {
     id : String,
-    value : "",
+    value : String,
     tick : Number,
     counter : Number
   };
-  const [todo, setTodo] = useState([layout]);
-  
+  const [todo, setTodo] = useState(localStorage.getItem('data')? JSON.parse(localStorage.getItem('data')): [layout]);
+  // useEffect(() => {
+  //   const storedData = JSON.parse(localStorage.getItem('data'));
+  //   if(storedData){
+  //     setTodo(storedData);
+  //   }
+  //   // return()=> {
+  //   //   localStorage.setItem('data', JSON.stringify(todo))
+  //   // }
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem('data', JSON.stringify(todo));
+  }, [tick]);
+
+
 
   function HandleAdd(e) {
     e.preventDefault();
+    if(value === "") {
+        return;
+    }
+    setTodo(todo => todo.filter((y)=> {
+      return y.value !== value;
+    })); 
     const new_id = crypto.randomUUID();
     setTodo(todo => [...todo, {id : new_id, value: value, tick : 0, counter : 0}]);
     setValue("");
     console.log(todo);
+  }
+
+  function HandleTick(x, tick_value, counter_value) {
+    x.tick = tick_value;
+    x.counter = counter_value;
+    setTick(!tick);
+  }
+
+  function HandleEdit(e, x) {
+    e.preventDefault();
+    if(revalue === "") {
+        x.counter = x.tick;
+        setTick(!tick);
+        return;
+    }
+    // setTodo(todo => todo.filter((y)=> {
+    //   return (y.value !== revalue);
+    // })); 
+    x.value=revalue;
+    x.counter=x.tick;
+    setRevalue("");
+    setTick(!tick);
+
   }
 
   return (
@@ -33,25 +76,25 @@ function App() {
         {todo.map((x, key)=> { 
           if(x.counter === 0 ) {
               return <li key={key}>
-                <input type='checkbox' checked={x.tick}  onChange={(e) => { x.tick =  1; x.counter = 1; setTick(!tick);}} />
-                 {x.value} 
-                <button onClick={(e) => { x.counter = 2; setTick((!tick));}}>
+                <input  type='checkbox' checked={x.tick}  onChange={(e) => { HandleTick(x, 1, 1)}} />
+                <div className='list-input'>{x.value} </div> 
+                <button onClick={(e) => { HandleTick(x, x.tick, 2)}}>
                   Edit
                 </button>
-                <button onClick={(e) => {x.counter = 3; setTick(!tick);}}>
-                  Deleted
+                <button onClick={(e) => {HandleTick(x, x.tick, 3)}}>
+                  Delete
                 </button>
               </li>  
           }
           else if( x.counter === 1) {
             return <li className='Completed-text' key={key}>
-              <input type='checkbox' checked={x.tick}  onChange={(e) => { x.tick = 0; x.counter = 0; setTick(!tick);  }} />
+              <input  type='checkbox' checked={x.tick}  onChange={(e) => { HandleTick(x, 0, 0)}} />
                {x.value} 
-              <button onClick={(e) => { x.counter = 2; setTick((!tick));}}>
+              <button onClick={(e) => { HandleTick(x, x.tick, 2)}}>
                 Edit
               </button>
-              <button onClick={(e) => {x.counter = 3; setTick(!tick);}}>
-                Deleted
+              <button onClick={(e) => {HandleTick(x, x.tick, 3)}}>
+                Delete
               </button>
               </li>
           }
@@ -59,12 +102,12 @@ function App() {
             return <li key={key}>
               <form >
                 <input type='checkbox' checked={x.tick} disabled/>
-                <input type='text' onChange={(e)=> { setRevalue(e.target.value)}}/>
-                <button type='submit' onClick={(e)=> {e.preventDefault(); x.value=revalue; x.counter=x.tick; setTick(!tick);}}>
+                <input className='list-input'  type='text' onChange={(e)=> { setRevalue(e.target.value)}}/>
+                <button type='submit' onClick={(e)=> {HandleEdit(e, x)}}>
                   Make Edits
                 </button>
                 <button onClick={(e) => {x.counter = 3; setTick(!tick)}} disabled>
-                  Deleted
+                  Delete
                 </button>
               </form>
               </li>
@@ -93,4 +136,15 @@ function App() {
 export default App;
 
 
+// const [data, setData] = useState([]);
+// useEffect(() => {
+//  const storedData = JSON.parse(localStorage.getItem('data'));
+//  if(storedData){
+//   setData(storedData);
+//  }
+// }, []);
+
+// useEffect(() => {
+//   localStorage.setItem('data', JSON.stringify(data));
+// }, [data]);
   
